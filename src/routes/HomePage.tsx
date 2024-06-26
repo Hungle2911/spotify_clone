8a2ba724578fbafcc7c19dd0fe9a213b3f0eff57
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-06-26 01:52:12 -0400

    feat: set up router

---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import HomeMain from "../components/HomeMain";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <HomeMain />
+    </>
+  );
+};
+
+export default HomePage;
